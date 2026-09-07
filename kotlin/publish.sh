5d9c0038471a0503a159b6fdc5d9e1152449895c
StrongTheDev <adhueywilan@gmail.com>
2026-09-07 16:25:48 +0300

    Update developer email and refine release process
    - Update email address in pom.xml
    - Disable generic release in JReleaser
    - Add optional dry-run mode to publish script

--- kotlin/publish.sh
+++ kotlin/publish.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-./gradlew clean && ./gradlew publish && ./gradlew jreleaserDeploy --stacktrace
+./gradlew clean && ./gradlew publish
+# allow a user to run "bash publish.sh skip" to skip deploying to mvn central.
+if [ -z "$1" ]; then
+    ./gradlew jreleaserDeploy --stacktrace
+else
+    ./gradlew jreleaserDeploy --dryrun --stacktrace
+fi
